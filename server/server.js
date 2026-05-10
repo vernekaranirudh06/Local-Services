0d640e5e88a291c9ec5369f4ffa282758c5cd90d
Anirudh Vernekar <vernekaranirudh06@gmail.com>
2026-05-10 19:01:26 +0530

    fix cors hardcoded

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const app = express()
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    process.env.FRONTEND_URL,
+    'https://local-services-3419.vercel.app',
   ],
   credentials: true,
 }))
